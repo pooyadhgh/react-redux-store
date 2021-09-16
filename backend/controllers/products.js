3a761379abaa203e622f4090d9e06cf3cf86b0f0
pooyadhgh <pooya.dhgh@gmail.com>
2021-09-16 11:45:12 +0430

    Enhance minor functionalities

---
+++ backend/controllers/products.js
@@ -0,0 +1,15 @@
+const products = require('../models/products');
+
+const getAllProducts = (req, res, next) => {
+  res.json(products);
+};
+
+const getProduct = (req, res, next) => {
+  const product = products.find(item => {
+    return item._id === req.params.id;
+  });
+
+  res.json(product);
+};
+
+module.exports = { getAllProducts: getAllProducts, getProduct: getProduct };
